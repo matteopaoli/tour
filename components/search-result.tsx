c62b924588d6c6f2b809eb70e8195c9152c5ed07
Matteo Paoli <matteo_paoli@outlook.com>
2022-12-31 04:25:32 +0700

    feat: remove trip page, import all bulms css, change serp card UI

--- components/search-result.tsx
+++ components/search-result.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image"
 import Link from "next/link"
 import { Trip } from "../types"
+import mockImage from '../public/sample.png'
 
 interface SearchResultProps {
   trip: Trip
@@ -7,10 +9,19 @@ interface SearchResultProps {
 
 const SearchResult = ({ trip }: SearchResultProps): JSX.Element => (
   <Link href={`/trip/${trip._id.toString()}`} className="box">
-    <h2 className="title is-4">{trip.points.map(x => x.name).join(' - ')}</h2>
-    <p className="subtitle is-6">{trip.dateStart.toString()}</p>
-    <p className="subtitle is-6">{trip.dateEnd.toString()}</p>
-    {/* <a className="is-size-7" href={trip.url}>{trip.url}</a> */}
+    <div className="columns is-vcentered">
+      <div className="column is-2">
+        <Image className="image is-96x96" src={mockImage} alt="sample image" />
+      </div>
+      <div className="column is-5">
+        <h2 className="title is-4">{trip.points[0].name}</h2>
+        <p className="subtitle is-6">{new Date(trip.dateStart).toTimeString()}</p>
+      </div>
+      <div className="column is-5">
+        <h2 className="title is-4">{trip.points[1].name}</h2>
+        <p className="subtitle is-6">{new Date(trip.dateEnd).toTimeString()}</p>
+      </div>
+    </div>
   </Link>
 )
 
