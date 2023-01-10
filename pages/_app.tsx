510702947539e9287d754eb760b5b73fd909c7cc
Matteo Paoli <matteo_paoli@outlook.com>
2023-01-10 10:53:58 +0700

    fix: remove useless import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/global.scss'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
-import BaseLayout from '../layouts/base-layout'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
