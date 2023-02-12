6ea171f8fbff809712d3b9f529bee4e1aa96e256
Matteo Paoli <matteo_paoli@outlook.com>
2023-02-12 23:44:38 +0700

    fix: git went crazy

--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,9 @@
-import Trip from "./trip"
-import ErrorResponse from "./error-response"
-import APIResponse from "./api-response"
-import StatusResponse from "./status-response"
-import Search from "./search"
-import LocationOption from "./location-option"
+import Trip from './trip'
+import ErrorResponse from './error-response'
+import APIResponse from './api-response'
+import StatusResponse from './status-response'
+import Search from './search'
+import LocationOption from './location-option'
 
 export type {
   APIResponse,
