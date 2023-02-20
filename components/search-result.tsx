ddd1f701cf8d4d718036b5384f8bbefdbf78ff96
Matteo Paoli <matteo_paoli@outlook.com>
2023-02-20 19:51:51 +0700

    feat: change ui search results

--- components/search-result.tsx
+++ components/search-result.tsx
@@ -28,20 +28,44 @@ const SearchResult = ({ trip }: SearchResultProps): JSX.Element => {
       </div>
       <div className="column is-10 columns is-vcentered">
         <div className="column is-one-third">
-          <h2 className="title is-5">{trip.points[0].name}</h2>
-          <p className="subtitle is-6">{formatDate(new Date(trip.dateStart))}</p>
+          <div className="columns is-mobile">
+            <div className="column is-9">
+              <h2 className="title is-5">{trip.points[0].name}</h2>
+            </div>
+            <div className="column is-3 is-hidden-tablet">
+              <p className="has-text-weight-bold is-half subtitle is-6">{new Date(trip.dateStart).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', 'hour12': false })}</p>
+            </div>
+          </div>
         </div>
         <div className="column is-one-third">
-          <h2 className="title is-5">{trip.points[1].name}</h2>
-          <p className="subtitle is-6">{formatDate(new Date(trip.dateEnd))}</p>
+          <div className="columns is-mobile">
+              <div className="column is-9">
+                <h2 className="title is-5">{trip.points[0].name}</h2>
+              </div>
+              <div className="column is-3 is-hidden-tablet">
+                <p className="has-text-weight-bold is-half subtitle is-6">{new Date(trip.dateEnd).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', 'hour12': false })}</p>
+              </div>
+            </div>
         </div>
         <div className="column is-one-third">
           {/* <h2 className="is-5 has-text-right">{trip.price}฿</h2> */}
+          <div className="columns is-vcentered is-hidden-mobile">
+            <div className="column">
+              <p className="has-text-weight-bold subtitle is-6">{new Date(trip.dateStart).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', 'hour12': false })}</p>
+            </div>
+            <div className="column">
+              <p className="is-one-third has-text-centered">{'->'}</p>
+            </div>
+            <div className="column">
+              <p className="has-text-weight-bold is-half subtitle is-6">{new Date(trip.dateEnd).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', 'hour12': false })}</p>
+            </div>
+          </div>
           <div className="button is-primary is-2 has-text-weight-bold is-fullwidth" onClick={onButtonClick}>{isSelected? <Image src={checkIcon as StaticImageData} alt="icon" />: 'Select this ticket'}</div>
         </div>
+        </div>
       </div>
-    </div>
   )
 }
 
 export default SearchResult
+
