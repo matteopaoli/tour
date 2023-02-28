cda916f327ab3d0ffbd43ba9e1f7440bc85e0202
Matteo Paoli <matteo_paoli@outlook.com>
2023-03-01 06:30:18 +0700

    style: lint

--- components/checkout-form/checkout-form.tsx
+++ components/checkout-form/checkout-form.tsx
@@ -35,7 +35,6 @@ export default function CheckoutForm() {
 
   const onSubmit = (data: CheckoutFormFields) => {
     data.dob = format(new Date(data.dob), 'yyyy-MM-dd')
-    console.log(data)
   }
 
   return (
